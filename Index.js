19ae9f4a647bd0de47d45d2c2def9b4caa046fd3
hajhouj <alaa.spread@gmail.com>
2026-09-19 21:05:47 +0100

    debut du projet

--- Index.js
+++ Index.js
@@ -1,26 +1,76 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql2/promise');
+
 
 
 app.use(cors());
+app.use(express.json());
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World !");
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '2001HAJHOUJREALMADRID$$$$',
+  database: 'database11',
 });
 
 
-app.post("/tester/",(req,res)=>{
-    console.log("hey")
-    const tableau=[]
-    for(let i=0;i<10;i++){
-        tableau.push("exemple"+i)
+app.post("/Ajouter",async(req,res)=>{
+    const body=req.body;
+
+    if(!body.name1 && !body.name2) {
+        res.send("tous les champs sont reduis")
+        return 
     }
 
-     res.send(tableau)
+    const {name1,name2}=body
+
+    const query=`insert into user (name1,name2) values ("${name1}","${name2}")`
+
+    try {
+        await pool.query(query)
+        res.send("opération passée avec succes")
+    } catch (error) {
+        console.log(error)
+        res.send("une erreur est servenue")
+    }
 })
 
+
+
+
+app.get("/recuperer",async(req,res)=>{
+   try {
+      const [rows]=await pool.query("select * from user")
+
+      console.log(rows)
+
+      res.send(rows)
+   } catch (error) {
+      console.log(error)
+      res.send("une erreur est servenue")
+   }
+})
+
+
+app.delete("/supprimer",async(req,res)=>{
+    
+  try {
+     const query="delete from User"
+
+     await pool.query(query)
+
+     console.log("suppression avec succes")
+
+     res.send("suppression avec succes")
+  } catch (error) {
+     console.log(error)
+     res.send("une erreur est servenue")
+  }
+})
 
 
 app.listen(4000, () => console.log("http://localhost:4000"));
